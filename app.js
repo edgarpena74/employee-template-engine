7d01ad728798afefb0f0a7131d40068d1480de2c
Lucah Endicott <lucahdavid88@gmail.com>
2020-12-09 13:04:14 -0800

    add descriptions to js files, update prompts

--- app.js
+++ app.js
@@ -10,38 +10,14 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
-
 const employeeArr = [];
 
+//function to first ask which type of employee the user would like to enter
 function getEmployeeTitle(){
     inquirer.prompt([
     {
       type: "list",
-      message: "What is this employee's job title?",
+      message: "Select employee's job title.",
       name: "titleInput",
       choices: ["Engineer", "Manager", "Intern"],
     },
@@ -58,6 +34,7 @@ function getEmployeeTitle(){
     })
 }
 
+//function to confirm whether or not the user would like to add a new employee
 function addNewEmp() {
     inquirer.prompt([
     {
@@ -75,6 +52,7 @@ function addNewEmp() {
     });
 };
 
+//function with add manager prompts
 function addManager () {
     inquirer.prompt([
     {
@@ -84,7 +62,7 @@ function addManager () {
     },
     {
         type: "input",
-        message: "Enter Manager ID.",
+        message: "Enter Manager employee ID.",
         name: "managerID",
     },
     {
@@ -106,6 +84,7 @@ function addManager () {
  })
 }
 
+//function with add engineer prompts
 function addEngineer () {
     inquirer.prompt([
         {
@@ -115,7 +94,7 @@ function addEngineer () {
         },
         {
             type: "input",
-            message: "Enter Engineer ID.",
+            message: "Enter Engineer employee ID.",
             name: "engineerID",
         },
         {
@@ -125,7 +104,7 @@ function addEngineer () {
         },
         {
             type: "input",
-            message: "Enter Engineer GitHub profile.",
+            message: "Enter Engineer GitHub user name.",
             name: "engineerGithub",
         },
       ])
@@ -137,6 +116,7 @@ function addEngineer () {
      })
     }
 
+//function with add intern prompts
 function addIntern () {
     inquirer.prompt([
         {
@@ -146,7 +126,7 @@ function addIntern () {
         },
         {
                 type: "input",
-                message: "Enter Intern ID.",
+                message: "Enter Intern employee ID.",
                 name: "internID",
         },
         {
@@ -168,6 +148,7 @@ function addIntern () {
     })
 }
 
+//function to write team.html once all employees are entered
 function renderMain () {
     const HTML = render(employeeArr);
     fs.writeFile(outputPath, HTML, (err) => {
